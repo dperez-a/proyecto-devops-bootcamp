6fd03f5365c1d3aa562776d61e980a7dc0f9832a
dperez-a <dpereza0503@gmail.com>
2026-02-03 10:26:15 +0100

    app.py uploaded

---
+++ app/models.py
@@ -0,0 +1,11 @@
+cat > app/models.py << 'EOF'
+from app import db
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f"<Data id={self.id} name={self.name}>"
+EOF
---
+++ run.py
@@ -0,0 +1,12 @@
+cat > run.py << 'EOF'
+import os
+
+from app import create_app
+
+
+env_name = os.getenv("FLASK_ENV", "development")
+app = create_app(env_name)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+EOF
